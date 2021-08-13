c01e51ddc73d42738b7bfd9616b17b115ba02168
Mateus Orsoni <matorsoni45@gmail.com>
2021-08-12 21:14:04 -0300

    Create main function (black window)

---
+++ src/main.cpp
@@ -0,0 +1,70 @@
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+#include <glad/glad.h>
+
+#include <iostream>
+
+// GLFW error calback.
+void error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Error: %s\n", description);
+}
+
+// GLFW key event callback.
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
+// Main function.
+int main()
+{
+    glfwSetErrorCallback(error_callback);
+
+    if (!glfwInit()) {
+        std::cout << "Failed to initialize GLFW." << std::endl;
+        return -1;
+    }
+
+    const int window_width = 640;
+    const int window_height = 480;
+    // Set minimum OpenGL version expected by the context.
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    GLFWwindow* window = glfwCreateWindow(window_width, window_height, "Demo", NULL, NULL);
+    if (!window) {
+        std::cout << "Failed to create window." << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGLLoader( (GLADloadproc)glfwGetProcAddress) ) {
+        std::cout << "Failed to initialize OpenGL context." << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwSwapInterval(1);
+    glfwSetKeyCallback(window, key_callback);
+
+    // Get frame buffer size and set OpenGL viewport dimensions.
+    // Should normally be the same size as the window.
+    {
+        int width, height;
+        glfwGetFramebufferSize(window, &width, &height);
+        glViewport(0, 0, width, height);
+    }
+
+    double tick = glfwGetTime(), tock;
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+}
