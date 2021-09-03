91d71da0bb5eaa7ca8dbfd7fe0fb05e2ddf750a1
Mateus Orsoni <matorsoni45@gmail.com>
2021-09-03 19:59:07 -0300

    Rename shader varible #2

--- src/shader/vertex.glsl
+++ src/shader/vertex.glsl
@@ -4,7 +4,7 @@ layout (location = 0) in vec3 in_pos;
 layout (location = 1) in vec3 in_normal;
 layout (location = 2) in vec2 in_tex;
 
-uniform mat4 u_mat;
+uniform mat4 u_view_projection;
 uniform mat4 u_model;
 
 out vec3 v_normal;
@@ -12,7 +12,7 @@ out vec2 v_tex;
 
 void main()
 {
-   gl_Position = u_mat * u_model * vec4(in_pos, 1.0);
+   gl_Position = u_view_projection * u_model * vec4(in_pos, 1.0);
    v_normal = in_normal;
    v_tex = in_tex;
 }
